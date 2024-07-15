061e974f7d9c81cebc008a0f78d805b44e279bdc
evannwang <35056037+evannwang@users.noreply.github.com>
2024-07-16 01:32:06 +0800

    [SCURM-18 19 20] inserting data to db from file completed

--- backend-python/main.py
+++ backend-python/main.py
@@ -1,7 +1,10 @@
 from app_config import app, db
 from flask_cors import CORS
-from flask import Flask
-from db_models.models import *
+
+from models import *
+from api_utility import *
+from mock_data_gen import *
+
 
 # Import any additional modules you created
 
@@ -9,7 +12,21 @@ def main():
     """Main function to run the Flask application."""
 
     # Run the Flask app
-    app.run(host='0.0.0.0', port=8000, debug=True)  # Set debug as per your development needs
+    app.run(host='0.0.0.0', port=8000, debug=True, use_reloader = False)  # Set debug as per your development needs
 
 if __name__ == '__main__':
+    # with app.app_context():
+        # db.create_all()  # This will create the database tables under the application context.
+        # shoes_rid = ["0E21922", "000000", "111111","222222"]
+        # customer_name = ["Spain Customer 36", "Milan Customer 30", "Spain Customer 39", "Spain Customer 40"]
+        # TestPopulateEndEntityDB(shoes_rid, customer_name)
+        # TestPopulateOrdersDB(firstTime=True, reset = False)
+    #with app.app_context():
+    with app.app_context():
+        # reset all tables increment id
+        datagen = MockDataGenerator(db)
+        datagen.cleanAndSetup()
+        datagen.initializeEntityDataPath()
+        datagen.setupDBLocalData(terminating_layer=5)
+
     main()
