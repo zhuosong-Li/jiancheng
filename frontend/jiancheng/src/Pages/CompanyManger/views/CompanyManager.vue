6a0659dcf6acfc35ef7b003942d1600eda5bf4af
zhuosong-Li <lzs97311@163.com>
2024-11-20 11:14:06 +0800

    companyPages Update

--- frontend/jiancheng/src/Pages/CompanyManger/views/CompanyManager.vue
+++ frontend/jiancheng/src/Pages/CompanyManger/views/CompanyManager.vue
@@ -1,92 +1,105 @@
 <template>
-  <el-container>
-    <el-header>
-      <AllHeader></AllHeader>
-    </el-header> <!--引用header-->
     <el-container>
-      <el-aside><!--引用aside-->
-        <div>
-          <el-avatar :icon="UserFilled" :size="100" />
-        </div>
-        <div style="font-size: x-large;">
-          {{ userName }}
-        </div>
-        <div class="aside-menu" style="width: 100%; margin-top: 50px;">
-          <el-menu default-active="1" class="el-menu-vertical-demo">
-            <el-menu-item index="1" @click="handleMenuClick('CostCalcAndProfitAnalysis')">
-              <span>成本计算与盈利分析</span>
-            </el-menu-item>
-            <el-menu-item index="2" @click="handleMenuClick('OrderStatusMonitor')">
-              <span>订单状态监控</span>
-            </el-menu-item>
-            <el-menu-item index="3" @click="handleMenuClick('MaterialPricesAndCostTrends')">
-              <span>材料价格与成本趋势</span>
-            </el-menu-item>
-            <el-menu-item index="4" @click="handleMenuClick('FinancialStatusAndDepartmentalInput')">
-              <span>财务状态与部门输入</span>
-            </el-menu-item>
-            <el-menu-item index="5" @click="handleMenuClick('OrderConfirmation')">
-              <span>订单确认</span>
-            </el-menu-item>
-            <el-menu-item index="9" @click="logout">
-              <span>退出系统</span>
-            </el-menu-item>
-          </el-menu>
-        </div>
-      </el-aside>
-      <el-main> 
-          <component :is="components[currentComponent]"></component>
-      </el-main>
+        <el-header>
+            <AllHeader></AllHeader>
+        </el-header>
+        <!--引用header-->
+        <el-container>
+            <el-aside
+                ><!--引用aside-->
+                <div>
+                    <el-avatar :icon="UserFilled" :size="100" />
+                </div>
+                <div style="font-size: x-large">
+                    {{ userName }}
+                </div>
+                <div class="aside-menu" style="width: 100%; margin-top: 50px">
+                    <el-menu default-active="1" class="el-menu-vertical-demo">
+                        <el-menu-item
+                            index="1"
+                            @click="handleMenuClick('CostCalcAndProfitAnalysis')"
+                        >
+                            <span>成本计算与盈利分析</span>
+                        </el-menu-item>
+                        <el-menu-item index="2" @click="handleMenuClick('OrderStatusMonitor')">
+                            <span>订单状态监控</span>
+                        </el-menu-item>
+                        <el-menu-item
+                            index="3"
+                            @click="handleMenuClick('MaterialPricesAndCostTrends')"
+                        >
+                            <span>材料价格与成本趋势</span>
+                        </el-menu-item>
+                        <el-menu-item
+                            index="4"
+                            @click="handleMenuClick('FinancialStatusAndDepartmentalInput')"
+                        >
+                            <span>财务状态与部门输入</span>
+                        </el-menu-item>
+                        <el-menu-item index="5" @click="handleMenuClick('OrderConfirmation')">
+                            <span>订单确认</span>
+                        </el-menu-item>
+                        <el-menu-item index="9" @click="logout">
+                            <span>退出系统</span>
+                        </el-menu-item>
+                    </el-menu>
+                </div>
+            </el-aside>
+            <el-main>
+                <component :is="components[currentComponent]"></component>
+            </el-main>
+        </el-container>
     </el-container>
-  </el-container>
 </template>
 
 <script setup lang="js">
-import AllHeader from '@/components/AllHeader.vue';
-import { UserFilled } from '@element-plus/icons-vue';
-import axios from 'axios';
-import { ref, onMounted } from 'vue';
-import CostCalcAndProfitAnalysis from "../components/CostCalcAndProfitAnalysis/CostCalcAndProfitAnalysis.vue";
-import OrderStatusMonitor from "../components/OrderStatusMonitor/OrderStatusMonitor.vue";
-import MaterialPricesAndCostTrends from "../components/MaterialPricesAndCostTrends/MaterialPricesAndCostTrends.vue";
-import FinancialStatusAndDepartmentalInput from "../components/FinancialStatusAndDepartmentalInput/FinancialStatusAndDepartmentalInput.vue";
-import OrderConfirmation from "../components/OrderConfirmation/OrderConfirmation.vue";
-import useSetAxiosToken from "../hooks/useSetAxiosToken";
+import AllHeader from '@/components/AllHeader.vue'
+import { UserFilled } from '@element-plus/icons-vue'
+import axios from 'axios'
+import { ref, onMounted, getCurrentInstance } from 'vue'
+import CostCalcAndProfitAnalysis from '../components/CostCalcAndProfitAnalysis/CostCalcAndProfitAnalysis.vue'
+import OrderStatusMonitor from '../components/OrderStatusMonitor/OrderStatusMonitor.vue'
+import MaterialPricesAndCostTrends from '../components/MaterialPricesAndCostTrends/MaterialPricesAndCostTrends.vue'
+import FinancialStatusAndDepartmentalInput from '../components/FinancialStatusAndDepartmentalInput/FinancialStatusAndDepartmentalInput.vue'
+import OrderConfirmation from '../components/OrderConfirmation/OrderConfirmation.vue'
+import useSetAxiosToken from '../hooks/useSetAxiosToken'
+import { useRouter } from "vue-router";
 
 const components = {
-  CostCalcAndProfitAnalysis,
-  OrderStatusMonitor,
-  MaterialPricesAndCostTrends,
-  FinancialStatusAndDepartmentalInput,
-  OrderConfirmation
-};
-let currentComponent = ref('CostCalcAndProfitAnalysis');
-let userName = ref('总经理');
-const {setAxiosToken} = useSetAxiosToken();
+    CostCalcAndProfitAnalysis,
+    OrderStatusMonitor,
+    MaterialPricesAndCostTrends,
+    FinancialStatusAndDepartmentalInput,
+    OrderConfirmation
+}
+let currentComponent = ref('CostCalcAndProfitAnalysis')
+let userName = ref('')
+const { setAxiosToken } = useSetAxiosToken()
+const $api_baseUrl = getCurrentInstance().appContext.config.globalProperties.$apiBaseUrl
+const router = useRouter();
 
 onMounted(() => {
-	setAxiosToken();
-	getUserAndCharacter();
-  handleMenuClick('CostCalcAndProfitAnalysis');
-});
+    setAxiosToken()
+    getUserAndCharacter()
+    handleMenuClick('CostCalcAndProfitAnalysis')
+})
 
 // 接口预留，请求后台获取当前登录用户信息
 async function getUserAndCharacter() {
-		// const response = await axios.get(`${$apiBaseUrl}`);
-		// userName = response.data.staffName + '-' + response.data.characterName;
+    const response = await axios.get(`${$api_baseUrl}/general/getcurrentstaffandcharacter`)
+    userName.value = response.data.staffName + '-' + response.data.characterName
 }
 
 // 菜单选项切换函数
 function handleMenuClick(value) {
-  currentComponent.value = value;
+    currentComponent.value = value
 }
 
 // 退出登录
 async function logout() {
-	await axios.post(`${$apiBaseUrl}/logout`)
-	localStorage.removeItem('token')
-	localStorage.removeItem('role')
-	router.push('/login')
+    await axios.post(`${$api_baseUrl}/logout`)
+    localStorage.removeItem('token')
+    localStorage.removeItem('role')
+    router.push('/login')
 }
-
 </script>
