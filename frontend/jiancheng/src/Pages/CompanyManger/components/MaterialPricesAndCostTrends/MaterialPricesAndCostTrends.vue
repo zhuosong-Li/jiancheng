b12673edeea53b745640a383f568dcdd723282b8
zhuosong-Li <lzs97311@163.com>
2024-11-20 10:54:35 +0800

    companyPages Update

--- frontend/jiancheng/src/Pages/CompanyManger/components/MaterialPricesAndCostTrends/MaterialPricesAndCostTrends.vue
+++ frontend/jiancheng/src/Pages/CompanyManger/components/MaterialPricesAndCostTrends/MaterialPricesAndCostTrends.vue
@@ -12,6 +12,8 @@
         >
             <component
                 :is="item.content"
+                :materialStorageId="item.materialStorageId"
+                :materialType="item.materialType"
                 :materialName="item.needQuery ? item.name : undefined"
             ></component>
         </el-tab-pane>
@@ -26,21 +28,26 @@ const editableTabsValue = ref('1');
 const editableTabs = ref([]);
 
 const handleTabsEdit = (targetName, action) => {
+    const tabName = targetName.split('>')[0];
+    const materialStorageId = targetName.split('>')[1]
+    const materialType = targetName.split('>')[2]
     if (action === 'add') {
         editableTabs.value.push({
-            title: targetName + '历史价格数据曲线',
-            name: targetName,
+            title: tabName + '历史价格数据曲线',
+            name: tabName,
             content: HistoryTrend,
             closable: true,
-            needQuery: true
+            needQuery: true,
+            materialStorageId: materialStorageId,
+            materialType: materialType
         });
-        editableTabsValue.value = targetName;
+        editableTabsValue.value = tabName;
     } else if (action === 'remove') {
         const tabs = editableTabs.value;
         let activeName = editableTabsValue.value;
-        if (activeName === targetName) {
+        if (activeName === tabName) {
             tabs.forEach((tab, index) => {
-                if (tab.name === targetName) {
+                if (tab.name === tabName) {
                     const nextTab = tabs[index + 1] || tabs[index - 1];
                     if (nextTab) {
                         activeName = nextTab.name;
@@ -51,7 +58,7 @@ const handleTabsEdit = (targetName, action) => {
             })
         }
         editableTabsValue.value = activeName;
-        editableTabs.value = tabs.filter((tab) => tab.name !== targetName);
+        editableTabs.value = tabs.filter((tab) => tab.name !== tabName);
     }
 }
 </script>
--- frontend/jiancheng/src/Pages/CompanyManger/components/OrderStatusMonitor/UnfinishedOrders.vue
+++ frontend/jiancheng/src/Pages/CompanyManger/components/OrderStatusMonitor/UnfinishedOrders.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="content">
-        <el-row :gutter="20" style="margin-top: 20px; justify-content: space-between;">
+        <el-row :gutter="16" style="margin-top: 20px; justify-content: space-between; width: 100%">
             <el-col :span="4" :offset="0" style="white-space: nowrap">
                 进行中订单号筛选：
-                <el-input
-                    v-model="orderRIdSearch"
-                    placeholder="请输入订单号"
-                    clearable
-                    @keypress.enter="updataParams('orderRId', orderRIdSearch)"
-                    @clear="updataParams('orderRId', orderRIdSearch)"
-                />
+                <el-input v-model="orderRIdSearch" placeholder="请输入订单号" clearable />
+                <el-button
+                    type="primary"
+                    size="middle"
+                    @click="updataParams('orderRid', orderRIdSearch)"
+                    style="margin-left: 20px"
+                    >筛选</el-button
+                >
             </el-col>
             <el-button type="primary" size="middle" @click="" :icon="Download"></el-button>
         </el-row>
@@ -41,32 +42,30 @@
                 </el-table-column>
             </el-table-column>
         </el-table>
-        <el-row :gutter="20" style="justify-content: end">
-            <el-col :span="7" :offset="8">
-                <el-pagination
-                    @size-change="chageCurrentPageSize"
-                    @current-change="changeCurrentPage"
-                    :current-page="currentPage"
-                    :page-sizes="[10, 20, 30, 40]"
-                    :page-size="currentPageSize"
-                    layout="total, sizes, prev, pager, next, jumper"
-                    :total="currentTotalRows"
-                />
-            </el-col>
+        <el-row :gutter="20" style="justify-content: end; width: 100%">
+            <el-pagination
+                @size-change="chageCurrentPageSize"
+                @current-change="changeCurrentPage"
+                :current-page="currentPage"
+                :page-sizes="[10, 20, 30, 40]"
+                :page-size="currentPageSize"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="currentTotalRows"
+            />
         </el-row>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { onMounted } from 'vue';
-import useTablePagination from '../../hooks/useTablePagination';
-import { Download } from '@element-plus/icons-vue';
+import { ref } from 'vue'
+import { onMounted } from 'vue'
+import useTablePagination from '../../hooks/useTablePagination'
+import { Download } from '@element-plus/icons-vue'
 
-const edit = defineEmits(['edit']);
+const edit = defineEmits(['edit'])
 
-let orderRIdSearch = ref('');
-const routeMsg = '';
+let orderRIdSearch = ref('')
+const routeMsg = ''
 const {
     currentPage,
     currentPageSize,
@@ -90,13 +89,8 @@ onMounted(() => {
             orderEndDate: ''
         }
     ]
-    updataParams('route', routeMsg);
+    updataParams('route', routeMsg)
 })
 </script>
 
-<style scoped>
-.content {
-    height: calc(100% - 40px);
-    width: calc(100% - 40px);
-}
-</style>
+<style scoped></style>
